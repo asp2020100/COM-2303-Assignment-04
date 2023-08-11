c31d1a366f99c09950a3031e5e83e7ee99658dca
R.M.Y.P.Rathnayake <ruslasp2020100@gmail.com>
2023-08-12 03:04:10 +0530

    contact us - delete functionality

--- contact-us-php/display-us.php
+++ contact-us-php/display-us.php
@@ -6,6 +6,13 @@ if (!isset($_SESSION['authenticated']) || !$_SESSION['authenticated']) {
     header('Location: login.php');
     exit;
 }
+// Logout functionality
+if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
+    session_unset();
+    session_destroy();
+    header('Location: login.php');
+    exit;
+}
 
 // Database connection settings
 $servername = "localhost";
@@ -20,6 +27,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+
+// Delete functionality
+
+if (isset($_GET['delete']) && isset($_GET['email'])) {
+    $deleteEmail = $_GET['email'];
+    $sql = "DELETE FROM contact_us WHERE email = '$deleteEmail'";
+    if ($conn->query($sql) === TRUE) {
+        // Record deleted successfully
+        header('Location: display-us.php');
+        exit;
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
 }
 
 // Retrieve data from the database
@@ -37,18 +58,25 @@ $conn->close();
     <link rel="stylesheet" type="text/css" href="display-us.css">
 </head>
 <body>
-    <h1>Saved Contact Data</h1>
+<div class="container">
+        <h1>Saved Contact Data</h1>
     <?php
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
+            echo "<div class='contact-info'>";
             echo "<p><strong>Name:</strong> " . $row["name"] . "<br>";
             echo "<strong>Email:</strong> " . $row["email"] . "<br>";
             echo "<strong>Message:</strong> " . $row["message"] . "</p>";
-            echo "<hr>";
+            echo "<a href='?delete=true&email=" . $row["email"] . "'>Delete</a>";
+            echo "</div>";
         }
     } else {
         echo "No data to display.";
     }
     ?>
+    <div class="logout">
+            <a href="?logout=true">Logout</a>
+        </div>
+    </div>
 </body>
 </html>
