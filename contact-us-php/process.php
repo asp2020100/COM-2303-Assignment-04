d7a37be9e4b3f4135070c55b175f7f4cdd340835
R.M.Y.P.Rathnayake <ruslasp2020100@gmail.com>
2023-08-12 02:22:03 +0530

    contact us php + sql

---
+++ contact-us-php/process.php
@@ -0,0 +1,32 @@
+<?php
+// Database connection settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contactme";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Prepare and execute the SQL query to insert data
+$sql = "INSERT INTO contact_us (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Message sent successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
